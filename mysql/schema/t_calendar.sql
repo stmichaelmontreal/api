9927044ab046341a52fdab20fd071d43d84bdbbd
Sergii Lutchyn <stmichaelmontreal@gmail.com>
2019-05-14 00:13:42 -0400

    script, db test 2 failed

--- mysql/schema/t_calendar.sql
+++ mysql/schema/t_calendar.sql
@@ -6,4 +6,4 @@ CREATE TABLE t_calendar (
   description mediumtext,
   when_created timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- mysql/schema/t_users.sql
+++ mysql/schema/t_users.sql
@@ -9,4 +9,4 @@ CREATE TABLE t_users (
   when_created timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
   UNIQUE KEY login_UNIQUE (login)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
